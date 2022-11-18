fb5450e3c50acf93c4f11bc0452a6fd342f276e2
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-11-19 00:45:22 +0900

    [add] 18. useQuery(custom hook)

---
+++ app/frontend/hooks/useQueryNews.ts
@@ -0,0 +1,26 @@
+import { request } from 'graphql-request'
+import { useQuery } from 'react-query'
+import { News } from '../types/types'
+import { GET_NEWS } from '../queries/queries'
+
+interface NewsRes {
+  news: News[]
+}
+
+export const fetchNews = async () => {
+  const { news: data } = await request<NewsRes>(
+    // undefined型はstring型に割り当てられないエラーが出るため、undefined型とstring型のユニオン型にした
+    process.env.NEXT_PUBLIC_HASURA_ENDPOINT || '',
+    GET_NEWS
+  )
+  return data
+}
+
+export const useQueryNews = () => {
+  return useQuery<News[], Error>({
+    // サーバーからフェッチしたデータをキャッシュに格納し、データを参照する
+    queryKey: 'news',
+    queryFn: fetchNews,
+    staleTime: Infinity,
+  })
+}
---
+++ app/frontend/hooks/useQueryTasks.ts
@@ -0,0 +1,39 @@
+import { useEffect } from 'react'
+import { GraphQLClient } from 'graphql-request'
+import { useQuery } from 'react-query'
+import { Task } from '../types/types'
+import { GET_TASKS } from '../queries/queries'
+import Cookie from 'universal-cookie'
+
+// クッキーを利用するための初期化
+const cookie = new Cookie()
+// Hasuraのエンドポイント
+const endpoint = process.env.NEXT_PUBLIC_HASURA_ENDPOINT || ''
+// GraphQLClientの初期化
+let graphqlClient: GraphQLClient
+
+// フェッチ後の返り値を型定義する
+interface TasksRes {
+  tasks: Task[]
+}
+
+const fetchTasks = async () => {
+  const { tasks: data } = await graphqlClient.request<TasksRes>(GET_TASKS)
+  return data
+}
+
+export const useQueryTasks = () => {
+  useEffect(() => {
+    graphqlClient = new GraphQLClient(endpoint, {
+      headers: {
+        Authorization: `Bearer ${cookie.get('token')}`,
+      },
+    })
+    // ログ切り替えやログアウトした場合に新しいGraphQLクライアントを生成し直し、JWTトークンをクッキーに保存し直す
+  }, [cookie.get('token')])
+  return useQuery<Task[], Error>({
+    queryKey: 'tasks',
+    queryFn: fetchTasks,
+    staleTime: 0,
+  })
+}
